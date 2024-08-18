ebee18dd07994283b118df3760a018d677263e42
rukayaj@gmail.com <rukayaj@gmail.com>
2024-08-18 23:43:42 +0200

    Fixed wonky front end

--- front-end/app/components/FunctionMessage.js
+++ front-end/app/components/FunctionMessage.js
@@ -4,17 +4,27 @@ import Collapse from 'react-bootstrap/Collapse';
 import { CodeBlock, dracula } from "react-code-blocks";
 
 
-function FunctionMessage({ message_content, message_id }) {
+function FunctionMessage({ message_content, message_id, is_python }) {
   const [open, setOpen] = useState(false);
+  let content = message_content
+  if(message_content.replace(/[ \t\n\r]/gm,'').startsWith('{"code":"')) { 
+    console.log('starts with code ' + message_id);
+    content = JSON.parse(message_content);
+    content = content['code']
+  }
+  console.log(content);
+
   return (
     <>
       <div className='inner-message python'>
-        <Button onClick={() => setOpen(!open)} aria-controls={`collapseFor${message_id}`} aria-expanded={open}>Show work</Button>
+        <Button onClick={() => setOpen(!open)} aria-controls={`collapseFor${message_id}`} aria-expanded={open} className={`${is_python ? ("code") : ("results")}`}>
+          {is_python ? ("Show generated code") : ("Show code results")}
+        </Button>
       </div>
       <div className="inner-function-message">
         <Collapse in={open}>
           <div id={`collapseFor${message_id}`}>
-            <CodeBlock text={message_content} language="python" theme={dracula} />
+            <CodeBlock text={content} language="python" theme={dracula} />
           </div>
         </Collapse>
       </div>
