2a91c1ef86d746a474ac5e842518ed0fc9968c06
rukayaj@gmail.com <rukayaj@gmail.com>
2024-10-03 06:26:16 +0200

    Tidy discord webhook away

--- back-end/api/helpers/discord_bot.py
+++ back-end/api/helpers/discord_bot.py
@@ -1,8 +1,9 @@
 import requests
+import os
 
 def send_discord_message(message):
     print(message)
-    response = requests.post('https://discord.com/api/webhooks/1275029272003743755/W9I7mielSalnXbr_Uvw_w_t6BZuiKNEGUiapF1XnH-AaKKJtvhCM2OlH90EYBCqj7mNQ', json={'content': message})
+    response = requests.post(os.getenv('DISCORD_WEBHOOK'), json={'content': message})
 
     if response.status_code == 204:
         print("Message sent successfully.")
