a85ce864a7560f60ac909bbea6b4712c4ea53392
rukayaj@gmail.com <rukayaj@gmail.com>
2024-10-03 05:57:32 +0200

    Bump

---
+++ back-end/api/helpers/discord_bot.py
@@ -0,0 +1,10 @@
+import requests
+
+def send_discord_message(message):
+    print(message)
+    response = requests.post('https://discord.com/api/webhooks/1275029272003743755/W9I7mielSalnXbr_Uvw_w_t6BZuiKNEGUiapF1XnH-AaKKJtvhCM2OlH90EYBCqj7mNQ', json={'content': message})
+
+    if response.status_code == 204:
+        print("Message sent successfully.")
+    else:
+        print(f"Failed to send message. Status code: {response.status_code}")
